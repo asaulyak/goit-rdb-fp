ef3509b9330318dab519fe6da363886cd990bd84
Eugene Asaulyak <eugene.asaulyak@gmail.com>
2024-05-31 13:44:00 +0300

    added scripts to fill tables with values

--- p2.sql
+++ p2.sql
@@ -22,3 +22,24 @@ create table infectious_cases_normalized
     constraint infectious_cases_normalized_regions_id_fk
         foreign key (region_id) references regions (id)
 );
+
+
+-- added scripts to fill out regions and infectious_cases_normalized tables
+insert into regions (entity, code) (select distinct Entity, Code from infectious_cases);
+
+insert into infectious_cases_normalized (year, number_yaws, polio_cases, cases_guinea_worm, number_rabies,
+                                         number_malaria, number_hiv, number_tuberculosis, number_smallpox,
+                                         number_cholera_cases, region_id)
+    (select year,
+            Number_yaws,
+            polio_cases,
+            cases_guinea_worm,
+            Number_rabies,
+            Number_malaria,
+            Number_hiv,
+            Number_tuberculosis,
+            Number_smallpox,
+            Number_cholera_cases,
+            r.id
+     from infectious_cases ic
+              inner join regions r on r.entity = ic.Entity);
